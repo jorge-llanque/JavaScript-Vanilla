1d78534207e43f5b5196673170fab325611bb7e1
jorge llanque (via Travis CI) <georgellanque@gmail.com>
2020-10-13 03:38:00 +0000

    Deploy jorge-llanque/JavaScript-Vanilla to github.com/jorge-llanque/JavaScript-Vanilla.git:gh-pages

---
+++ src/pages/Error404.js
@@ -0,0 +1,11 @@
+const Error404 = () => {
+    const view =
+        `
+        <div class="Error 404">
+            <h2>Lo siento, esta página no está disponible. Error 404</h2>
+        </div>
+    `;
+    return view;
+}
+
+export default Error404;
